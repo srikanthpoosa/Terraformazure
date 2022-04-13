ad518143bef0cb9b6cbffc70951db66772d28f59
srikanthpoosa <77892649+srikanthpoosa@users.noreply.github.com>
2022-04-13 09:14:06 +0530

    added missing modules

--- main.tf
+++ main.tf
@@ -19,21 +19,21 @@ resource "azurerm_resource_group" "example" {
   location = "SouthIndia"
 }
 
-resource "azurerm_virtual_network" "azurevnet" {
+resource "azurerm_virtual_network" "example" {
   name                = "example-network-net"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "azure-subnet" {
+resource "azurerm_subnet" "example" {
   name                 = "internal-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "azure-nsg" {
+resource "azurerm_network_interface" "example" {
   name                = "example-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -45,7 +45,7 @@ resource "azurerm_network_interface" "azure-nsg" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "azure-vm" {
+resource "azurerm_linux_virtual_machine" "example" {
   name                = "tfvm"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
